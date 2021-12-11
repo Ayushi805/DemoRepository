f7103df284649459cafbfc0932109b6ef1e03095
ayush <ayush@DESKTOP-4MKVI18>
2021-12-11 20:25:47 +0530

    First Commit

---
+++ Demo/src/Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hi! I'm Ayushi! This is the first line of code");
+	}
+
+}
